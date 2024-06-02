5daf77015cc3be233b1e9b43d76f7c07d8e13695
ajaykumargdr <ajaykumarncas@gmail.com>
2024-06-02 11:21:47 +0530

    doc: add comments in each arguments of cli commands

--- src/cli/commands.rs
+++ src/cli/commands.rs
@@ -51,55 +51,93 @@ impl Cli {
 pub enum Commands {
     // Todo(return secret phrase)
     #[command(about = "Creates/Initialize the wallet")]
-    Init { endpoint: String },
+    Init {
+        /// The endpoint to connect to
+        endpoint: String,
+    },
 
     #[command(about = "Creates a new account")]
-    Create { account_name: String },
+    Create {
+        /// A new name for the account to be created
+        account_name: String,
+    },
 
     #[command(about = "Imports an existing account")]
     Import {
+        /// A new name for the account to be imported
         account_name: String,
 
+        /// The secret key of the account to be imported
         #[command(subcommand)]
         secret: SecretKey,
     },
 
     #[command(about = "Exports an existing account to a file")]
-    Export { account_name: String, path: PathBuf },
+    Export {
+        /// The name of the account to be exported
+        account_name: String,
+
+        /// The path to export the account to
+        path: PathBuf,
+    },
 
     #[command(about = "Gets the balance of an account")]
-    Balance { account_name: String },
+    Balance {
+        /// The name of the account to get the balance of
+        account_name: String,
+    },
 
     #[command(about = "Gets the balance of an account")]
-    Id { account_name: String },
+    Id {
+        /// The name of the account to get the public address of
+        account_name: String,
+    },
 
     #[command(about = "Makes a new transaction to an account")]
     Transfer {
+        /// The name of the account to transfer from
         account_name: String,
+
+        /// Public address of the account to transfer to
         to: String,
+
+        /// Amount of ETH to transfer
         eth: f64,
     },
 
     #[command(about = "Lists all account names along with their public addresses")]
     List {
+        /// Get the balance of all accounts
         #[arg(long, short, action)]
         balance: bool,
     },
 
     #[command(about = "Renames a account name")]
     Rename {
+        /// The name of the account to be renamed
         old_account_name: String,
+
+        /// The new name of the account
         new_account_name: String,
     },
 
     #[command(about = "Renames a account name")]
-    Remove { account_name: String },
+    Remove {
+        /// The name of the account to be removed
+        account_name: String,
+    },
 }
 
 #[derive(Debug, Clone, Parser)]
 pub enum SecretKey {
     #[command(about = "Import from file")]
-    File { path: PathBuf },
+    File {
+        /// The path to the secret key file
+        path: PathBuf,
+    },
     #[command(about = "Import from secret key")]
-    Key { secret_key: String },
+    Key {
+        /// The secret key to import
+        secret_key: String,
+    },
 }
